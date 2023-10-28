2978b3e43c1756c7266db26ebce5d71a36bc2e0e
Michael Johansson <mike@gofyeo.com>
2023-10-28 22:42:14 +0200

    Install Python with pyenv

--- bootstrap/bootstrap-system.sh
+++ bootstrap/bootstrap-system.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env zsh
 
+PYTHON_VERSION="3.11.4"
+
+
 # Copy bin folder to $HOME
 cp -r ./bin ~/bin
 
 # Install Oh-My-Zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 cp ../.zshrc ~/.zshrc
+
+# Install Python
+pyenv install $PYTHON_VERSION
