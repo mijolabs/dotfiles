e242dddfe5a0436c011985445f2f1828f9719d5c
Michael Johansson <mike@gofyeo.com>
2023-10-28 21:01:38 +0200

    Facilitate version control management

--- .oh-my-zsh/custom/go.zsh
+++
@@ -1,4 +0,0 @@
-export GOPATH=$HOME/code/dev/go
-export PATH=$PATH:$GOPATH/bin
-export PATH=$PATH:$GOROOT/bin
-
--- macos-fixes.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env zsh
-
-# Disable creation of `.DS_Store` files on network drives and external disks
-defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
-defaults write com.apple.desktopservices DSDontWriteUSBStores -bool true
-
